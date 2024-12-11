f78ffe2b28d0372dcae6aaf4286cac43d9b5f2f8
khwankamon <khwan_yyg@outlook.com>
2024-12-11 13:18:31 +0700

    connect to MongoDB

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
-import { sample_foods, sample_tags, sample_users } from "./data";
-import { FRONTEND_URL } from "./constants/urls";
-import jwt from "jsonwebtoken";
+import foodRouter from "./routers/food.router";
+import userRouter from "./routers/user.router";
+import { dbConnect } from "./configs/database.config";
+
+dbConnect();
 
 const app = express();
 
@@ -11,72 +16,13 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: [FRONTEND_URL],
+    origin: [process.env.FRONTEND_URI || "http://localhost:4200"],
     methods: ["POST", "GET"],
   })
 );
 
-// routes
-app.get("/", (req, res) => {
-  res.json({ mssg: "Welcome to the Food Store app" });
-});
-
-app.get("/api/foods", (req, res) => {
-  res.json(sample_foods);
-});
-
-app.get("/api/foods/search/:searchTerm", (req, res) => {
-  const searchTerm = req.params.searchTerm;
-  const foods = sample_foods.filter((food) =>
-    food.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
-  res.send(foods);
-});
-
-app.get("/api/foods/tags", (req, res) => {
-  res.send(sample_tags);
-});
-
-app.get("/api/foods/tag/:tagName", (req, res) => {
-  const tagName = req.params.tagName;
-  const foods = sample_foods.filter((food) => food.tags?.includes(tagName));
-  res.send(foods);
-});
-
-app.get("/api/foods/:foodId", (req, res) => {
-  const foodId = req.params.foodId;
-  const food = sample_foods.find((food) => food.id == foodId);
-  res.send(food);
-});
-
-app.post("/api/users/login", (req, res) => {
-  const { email, password } = req.body;
-  const user = sample_users.find(
-    (user) => user.email === email && user.password === password
-  );
-
-  if (user) {
-    res.send(generateTokenResponse(user));
-  } else {
-    res.status(400).send("Username or password is not valid!");
-  }
-});
-
-const generateTokenResponse = (user: any) => {
-  const token = jwt.sign(
-    {
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
-    "SomeRandomText",
-    {
-      expiresIn: "30d",
-    }
-  );
-
-  user.token = token;
-  return user;
-};
+app.use("/api/foods", foodRouter);
+app.use("/api/users", userRouter);
 
 const port = 5000;
 app.listen(port, () => {
