c322be31f1bdd2f2223f0803a19d2ac6766907e1
khwankamon <khwan_yyg@outlook.com>
2024-12-11 10:55:25 +0700

    add login feature with toastr, jsonwebtoken

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { AppRoutingModule } from "./app-routing.module";
-import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./components/partials/header/header.component";
-import { HomeComponent } from "./components/pages/home/home.component";
-import { StarRatingComponent } from "./components/partials/star-rating/star-rating.component";
-import { SearchComponent } from "./components/partials/search/search.component";
-import { TagsComponent } from "./components/partials/tags/tags.component";
-import { FoodPageComponent } from "./components/pages/food-page/food-page.component";
-import { CartPageComponent } from "./components/pages/cart-page/cart-page.component";
-import { TitleComponent } from "./components/partials/title/title.component";
-import { NotFoundComponent } from "./components/partials/not-found/not-found.component";
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
+import { SearchComponent } from './components/partials/search/search.component';
+import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
+import { TextInputComponent } from './components/partials/text-input/text-input.component';
+import { DefaultButtonComponent } from './components/partials/default-button/default-button.component';
 
 @NgModule({
   declarations: [
@@ -26,8 +34,24 @@ import { NotFoundComponent } from "./components/partials/not-found/not-found.com
     CartPageComponent,
     TitleComponent,
     NotFoundComponent,
+    LoginPageComponent,
+    InputContainerComponent,
+    InputValidationComponent,
+    TextInputComponent,
+    DefaultButtonComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      newestOnTop: false,
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- frontend/src/app/components/partials/header/header.component.ts
+++ frontend/src/app/components/partials/header/header.component.ts
@@ -1,16 +1,32 @@
-import { Component } from "@angular/core";
-import { CartService } from "src/app/services/cart.service";
+import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.css"],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
   cartQuantity = 0;
-  constructor(cartService: CartService) {
+  user!: User;
+
+  constructor(cartService: CartService, private userService: UserService) {
     cartService.getCartObservable().subscribe((newCart) => {
       this.cartQuantity = newCart.totalCount;
     });
+
+    userService.userObservable.subscribe((newUser) => {
+      this.user = newUser;
+    });
+  }
+
+  logout() {
+    this.userService.logout();
+  }
+
+  get isAuth() {
+    return this.user.token;
   }
 }
