6536ad8120c86d42774dfe7387b1c01c63a785d0
khwankamon <khwan_yyg@outlook.com>
2024-12-18 11:18:57 +0700

    create order with auth

---
+++ backend/src/middlewares/auth.mid.ts
@@ -0,0 +1,16 @@
+import { verify } from "jsonwebtoken";
+import { HTTP_UNAUTHORIZED } from "../constants/http_status";
+
+export default (req: any, res: any, next: any) => {
+  const token = req.headers.access_token as string;
+  if (!token) return res.status(HTTP_UNAUTHORIZED).send();
+
+  try {
+    const decodeUser = verify(token, process.env.JWT_SECRET!);
+    req.user = decodeUser;
+  } catch (error) {
+    res.status(HTTP_UNAUTHORIZED).send();
+  }
+
+  return next();
+};
